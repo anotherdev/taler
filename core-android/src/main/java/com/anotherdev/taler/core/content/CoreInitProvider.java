2e6a2e220ee6c99191a6fffce733b4b1d505816e
Polson Keeratibumrungpong <polson@Polsons-iMac.local>
2017-07-06 00:14:45 +0700

    plant crashlytics timber tree

--- core-android/src/main/java/com/anotherdev/taler/core/content/CoreInitProvider.java
+++ core-android/src/main/java/com/anotherdev/taler/core/content/CoreInitProvider.java
@@ -1,14 +1,18 @@
 package com.anotherdev.taler.core.content;
 
+import com.anotherdev.taler.analytics.log.CrashlyticsTree;
 import com.crashlytics.android.Crashlytics;
 
 import io.fabric.sdk.android.Fabric;
+import timber.log.Timber;
 
 public class CoreInitProvider extends BaseInitProvider {
 
     @Override
     public boolean onCreate() {
         Fabric.with(getContext(), new Crashlytics());
+
+        Timber.plant(new CrashlyticsTree());
         return false;
     }
 }
