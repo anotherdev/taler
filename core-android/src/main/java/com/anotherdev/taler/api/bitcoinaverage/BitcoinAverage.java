76c18b9921c3e8a70b72c2bb07f6bd80db522b24
Polson Keeratibumrungpong <polson@Polsons-iMac.local>
2017-07-15 23:24:33 +0700

    init api singleton

---
+++ core-android/src/main/java/com/anotherdev/taler/api/bitcoinaverage/BitcoinAverage.java
@@ -0,0 +1,28 @@
+package com.anotherdev.taler.api.bitcoinaverage;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class BitcoinAverage {
+
+    private static final String BASE_URL = "https://apiv2.bitcoinaverage.com/";
+
+    private static class ApiHolder {
+        private static final BitcoinAverageApi API = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
+                .build()
+                .create(BitcoinAverageApi.class);
+    }
+
+
+    private BitcoinAverage() {
+        // classic singleton for now instead of dagger
+    }
+
+    public static BitcoinAverageApi getApi() {
+        return ApiHolder.API;
+    }
+}
--- core-android/src/main/java/com/anotherdev/taler/api/bitcoinaverage/BitcoinAverageApi.java
+++ core-android/src/main/java/com/anotherdev/taler/api/bitcoinaverage/BitcoinAverageApi.java
@@ -2,11 +2,11 @@ package com.anotherdev.taler.api.bitcoinaverage;
 
 import com.anotherdev.taler.api.bitcoinaverage.model.Response;
 
-import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.http.GET;
 
 public interface BitcoinAverageApi {
 
     @GET("constants/symbols")
-    Observable<Response> getSymbols();
+    Single<Response> getSymbols();
 }
